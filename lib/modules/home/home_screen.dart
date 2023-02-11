b2f38862f684fe75866b51b2398dfc4d25905fb9
veeery <very.very.23.5.1999@gmail.com>
2023-02-11 19:30:30 +0700

    add: key Geolocation from GoogleConsole Map SDK

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:nearest/constant/app_size_utils.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,8 +10,24 @@ class HomeScreen extends StatelessWidget {
     AppSizeUtil.init(context: context);
 
     return Scaffold(
-      body: Center(
-        child: Text('Home', style: TextStyle(fontFamily: 'Poppins')),
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
+        child: Container(
+          width: 100.w,
+          height: 100.h,
+          color: Colors.red,
+          child: Column(
+            children: [
+              Container(
+                width: 50.w,
+                height: 50.h,
+                child: GoogleMap(
+                    initialCameraPosition:
+                        CameraPosition(target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.476)),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
